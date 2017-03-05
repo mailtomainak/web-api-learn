2c523df1f35d865930df01c27234f81fb86a82e0
mainak <mailtomainak@gmail.com>
2017-03-05 17:48:56 +0530

    remove product interface
    remove an extra interface

--- models/Product.cs
+++ models/Product.cs
@@ -1,17 +1,11 @@
 ﻿
 namespace models
 {
-   public class Product:IProduct
+    public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsActive { get; set; }
     }
-
-    public interface IProduct
-    {
-         int Id { get; set; }
-         string Name { get; set; }
-         bool IsActive { get; set; }
-    }
 }
+   
